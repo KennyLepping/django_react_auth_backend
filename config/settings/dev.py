d22612d04755a95a9dc9aa5889e64ae0e95b8c35
Kenny Lepping <kennylepping@gmail.com>
2022-01-24 16:40:52 -0500

    First commit

---
+++ config/settings/dev.py
@@ -0,0 +1,15 @@
+from .base import *
+
+SECRET_KEY = 'django-insecure-44%u*i4*8()vqdzcw(7!j4vaqp%9&e8x*1+gw)@hw^&(!q+^ve'
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_URL = 'media/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
