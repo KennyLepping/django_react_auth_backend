130489f696baf378c9e2933137f987dcf372bb40
Kenny Lepping <kennylepping@gmail.com>
2022-01-29 23:42:47 -0500

    Added comma to CORS settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -132,7 +132,7 @@ REST_FRAMEWORK = {
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "https://d3t7z2ygpu11ag.cloudfront.net"
+    "https://d3t7z2ygpu11ag.cloudfront.net",
     "https://www.kennystestdomain.de"
     # TODO: FRONTEND URL HERE
 ]
